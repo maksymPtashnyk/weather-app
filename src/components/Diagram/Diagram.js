eaae83cf1bf7f8b2751c511c9757d245f6978445
maksymPtashnyk <super.zet65@gmail.com>
2024-02-01 22:58:32 +0200

    fixed styles

--- src/components/Diagram/Diagram.js
+++ src/components/Diagram/Diagram.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './Diagram.css';
+import { format, parseISO } from 'date-fns';
 import { ResponsiveContainer, AreaChart, Area, XAxis, YAxis, LabelList } from 'recharts';
 import { fetchWeatherForecastData } from '../../api/weatherApi';
 
 const formatDate = (dateString) => {
-  const date = new Date(dateString + ' UTC');
-  const localDate = new Date(date.toLocaleString('en-US', { timeZone: 'Europe/Kyiv' }));
-  const day = localDate.getDate();
-  const month = localDate.getMonth() + 1;
-  return `${day < 10 ? '0' : ''}${day}.${month < 10 ? '0' : ''}${month}`;
+  try {
+    const parsedDate = parseISO(dateString);
+    
+    if (isNaN(parsedDate.getTime())) {
+      console.error('Invalid date:', dateString);
+      return ''; // or handle the error in a way that makes sense for your application
+    }
+
+    return format(parsedDate, 'dd.MM');
+  } catch (error) {
+    console.error('Error formatting date:', error);
+    return ''; // or handle the error in a way that makes sense for your application
+  }
 };
 
 const WeatherForecastChart = ({ city, temp }) => {
@@ -31,7 +40,7 @@ const WeatherForecastChart = ({ city, temp }) => {
   const getColor = (temperature) => (temperature < 0 ? '#5B8CFF' : '#FFA25B');
 
   return (
-    <ResponsiveContainer width="100%" height={150} className='box'>
+    <ResponsiveContainer width="100%" height={220} className='box'>
       <AreaChart data={data}>
         <defs>
           <linearGradient id={gradientId} x1="0" y1="0" x2="0" y2="1">
@@ -39,10 +48,10 @@ const WeatherForecastChart = ({ city, temp }) => {
             <stop offset="95%" stopColor={getColor(temp)} stopOpacity={0} />
           </linearGradient>
         </defs>
-        <YAxis width={16} interval={0} className='y-axis'/>
-        <XAxis dataKey="dt_txt" tickMargin={10} tickFormatter={formatDate} axisLine={false} tickLine={false} height={50} className='data' margin={{ left: 20, right: 20 }} />
-        <Area type="monotone" dataKey="main.temp" fill={`url(#${gradientId})`} baseValue={'dataMin'} strokeWidth={0} className='area'>
-          <LabelList dataKey="main.temp" position="top" className='temperature' />
+        <YAxis className='y-axis' width={10} opacity={0}/>
+        <XAxis dataKey="dt_txt" tickMargin={5} tickFormatter={formatDate} axisLine={false} tickLine={false} height={50} className='data' interval={'preserveStartEnd'} />       
+         <Area type="monotone" dataKey="main.temp" fill={`url(#${gradientId})`} baseValue={'dataMin'} strokeWidth={0} className='area'>
+         <LabelList dataKey="main.temp" position={{ x: 0, y: 0 }} className='temperature-text' />
         </Area>
       </AreaChart>
     </ResponsiveContainer>
